51f9f7359c83d421d568a034eae2b99b74e631d4
BudyaSH <andrewbudy@gmail.com>
2014-10-02 02:48:08 +0300

    Lab1 test 1

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -11,8 +11,9 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setResizable(false);
+        primaryStage.setTitle("Generation of Erlang n-th Flow ");
+        primaryStage.setScene(new Scene(root, 990, 742));
         primaryStage.show();
     }
 
