f6c30b3f205adc832362945a1a6433c789fe3fd8
BudyaSH <andrewbudy@gmail.com>
2014-10-08 11:52:46 +0300

    Last v with AutoCorrelation present

--- Lab1/src/AnalyzeMain/Analyzer.java
+++ Lab1/src/AnalyzeMain/Analyzer.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 
 public class Analyzer {
 
-    public static double[] Analyze(ArrayList<Double> arr){
+    public static double[] analyze(ArrayList<Double> arr){
 
         double[] res = new double[2];
         double sum = 0;
@@ -26,5 +26,39 @@ public class Analyzer {
         res[1] = k;
 
         return res;
+    }
+
+    public static double calcAC(ArrayList<Double> arr,int p){
+        double ac;
+        int N = arr.size();
+        double sum = 0;
+        //1-st MathExp
+        for (int i = p; i < N; i++) {
+            sum += arr.get(i);
+        }
+        double m1 = sum /(N-p);
+
+        //2-nd MathExp
+        sum = 0;
+        for (int i = 0; i < N; i++) {
+            sum += arr.get(i);
+        }
+        double m2 = sum /(N-p);
+
+        //AC calculation
+        sum = 0;
+        double suml1 = 0;
+        double suml2 = 0;
+
+        for (int i = p; i < N; i++) {
+            sum += (arr.get(1) -m1 )*(arr.get(i-p)-m2);
+            suml1 += (arr.get(i)-m1)*(arr.get(i)-m1);
+            suml2 += (arr.get(i-p)-m2)*(arr.get(i-p)-m2);
+        }
+        ac = sum/Math.sqrt(suml1*suml2);
+
+
+
+        return ac;
     }
 }
--- Lab1/src/AnalyzeMain/Controller.java
+++ Lab1/src/AnalyzeMain/Controller.java
@@ -4,8 +4,6 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
-import javafx.scene.text.Text;
 import javafx.stage.FileChooser;
 
 import java.io.*;
@@ -20,30 +18,24 @@ public class Controller {
     @FXML
     private Label lField;
     @FXML
+    private Label aCField;
+    @FXML
     private Button genBtn;
-    private Scanner s = null;
+    @FXML
+    private Button aCBtn;
     @FXML
     private Label kText;
     @FXML
+    private Label aCText;
+    @FXML
     private Label lText;
     private File output_file;
-
+    private ArrayList<Double> values;
+    private Scanner s = null;
 
     @FXML protected void handleGenerateButtonAction(ActionEvent event) {
-        String eol = System.getProperty("line.separator");
-        ArrayList<Double> values = new ArrayList<Double>();
-        try {
-            s = new Scanner(output_file);
-            while (s.hasNext()) {
-                values.add(Double.parseDouble(s.next()));
-            }
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        }
-        finally {
-            s.close();
-        }
-        double[] d = Analyzer.Analyze(values);
+        values = getValues();
+        double[] d = Analyzer.analyze(values);
         lText.setVisible(true);
         kText.setVisible(true);
         kField.setText(String.format("%.2f",d[1]));;
@@ -61,10 +53,36 @@ public class Controller {
         if (selectedFile != null) {
             output_file = selectedFile;
            genBtn.setDisable(false);
+           aCBtn.setDisable(false);
         }
 
 
 
 
+    }
+
+    @FXML protected void handleCalcAutoCorButtonAction(ActionEvent event) {
+        values = getValues();
+        aCText.setVisible(true);
+        double ac = Analyzer.calcAC(values,1);
+        aCField.setText(String.format("%.8f",ac));
+
+
+    }
+
+    private ArrayList<Double> getValues(){
+        ArrayList<Double> values = new ArrayList<Double>();
+        try {
+            s = new Scanner(output_file);
+            while (s.hasNext()) {
+                values.add(Double.parseDouble(s.next()));
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        finally {
+            s.close();
+        }
+        return values;
     }
 }
